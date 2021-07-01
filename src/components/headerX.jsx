34ae5e03d5fffbdfa0e1c78fea06d4a3113d3fb3
Lukas <lukas.ancikevicius@gmail.com>
2021-07-01 10:48:49 +0300

    shop cat + social+ pass routes props

--- src/components/headerX.jsx
+++ src/components/headerX.jsx
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 class HeaderX extends Component {
   render() {
     return (
-      <header>
-        <h1>I am a header</h1>
+      <header className="header">
+        <div className="container d-flex space-between">
+          <Link to="/" className="logo">
+            My
+            <strong>ShopX</strong>
+          </Link>
+          <nav className="main-nav">
+            {this.props.navLinks.map((n) => {
+              return (
+                <Link key={n.title} className="nav-link-item" to={n.to}>
+                  {n.title}
+                </Link>
+              );
+            })}
+          </nav>
+        </div>
       </header>
     );
   }
