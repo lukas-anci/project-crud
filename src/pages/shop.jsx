d5b6d26b84041b5d704948005c83566ca16558fe
Lukas <lukas.ancikevicius@gmail.com>
2021-07-01 11:27:46 +0300

    npm fontaws, mapped categories and icons

--- src/pages/shop.jsx
+++ src/pages/shop.jsx
@@ -1,9 +1,34 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 class Shop extends Component {
   render() {
+    const { shopCategories, socialLink } = this.props.shop;
     return (
       <div className="shop-page">
-        <h1>I am a shop</h1>
+        <aside className="categories-aside">
+          <div className="categories">
+            <ul>
+              {shopCategories.map((item) => {
+                return (
+                  <li className="category-item" key={item._id}>
+                    {item.title}
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+          <div className="social">
+            <ul>
+              {socialLink.map((item) => {
+                return (
+                  <Link key={item.icon} className="nav-link-item" to={item.to}>
+                    <i className={item.icon}></i>
+                  </Link>
+                );
+              })}
+            </ul>
+          </div>
+        </aside>
       </div>
     );
   }
