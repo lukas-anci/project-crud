59f9860a982f5d8fc90eae8a209870661c2de5c0
Lukas <lukas.ancikevicius@gmail.com>
2021-07-02 11:54:13 +0300

    style single info part

--- src/components/shopSingleItem.jsx
+++ src/components/shopSingleItem.jsx
@@ -12,35 +12,49 @@ class ShopSingleItem extends Component {
   }
   componentDidMount() {
     const { item } = this.props;
+
     const imagesRequired = item.images.map(
       (imgNo) =>
-        require(`../static/shop/hats/${item.image}${imgNo}.jpg`).default
+        require(`../static/shop/hats/${item.image}_${imgNo}.jpg`).default
     );
 
     // set default image
-    this.setState({ images: imagesRequired });
+    // main imaage component
+    // onclick to show foto
+
+    this.setState({ images: imagesRequired, mainImage: imagesRequired[2] });
   }
 
+  handleMainImage = (img) => {
+    this.setState({ mainImage: img });
+  };
+
   render() {
-    const { socialLinksData } = this.props;
+    const { socialLinksData, item } = this.props;
     return (
       <div className="single-item ">
         <div className="d-flex">
           <div className="single__images-part w-50">
             <img
               className="single__main-image"
-              src="https://picsum.photos/640/480"
+              src={this.state.mainImage}
               alt="main item"
             />
             <div className="single__photos d-flex flex-wrap">
               {this.state.images.map((img) => (
-                <img key={img} src={img} alt="small item" />
+                <img
+                  className="single__item-image"
+                  onClick={() => this.handleMainImage(img)}
+                  key={img}
+                  src={img}
+                  alt="small item"
+                />
               ))}
             </div>
           </div>
           <div className="single__item-info-part">
-            <h2 className="item-info__title">title</h2>
-            <p className="item-info__price">300 eur</p>
+            <h2 className="item-info__title">{item.title}</h2>
+            <p className="item-info__price">{item.price}</p>
             <div className="item-info__options d-flex">
               <div>
                 <label htmlFor="colors">Colors</label> <br />
@@ -59,8 +73,7 @@ class ShopSingleItem extends Component {
                 </select>
               </div>
             </div>
-
-            <Button outline>Add to cart</Button>
+            <Button outline>Add to cart</Button> <br />
             <Button>Buy it now</Button>
             <SocialLinks titles socialLink={socialLinksData} />
           </div>
