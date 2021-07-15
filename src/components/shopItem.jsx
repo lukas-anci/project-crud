3bd5c60ff3d1e68182cae58df610c27bb194b8d4
Lukas <lukas.ancikevicius@gmail.com>
2021-07-15 15:39:45 +0300

    sold ou shop item

--- src/components/shopItem.jsx
+++ src/components/shopItem.jsx
@@ -4,16 +4,24 @@ import Price from './common/price';
 
 class ShopItem extends Component {
   render() {
-    const { _id, price, title, image, salePrice } = this.props.item;
+    const { _id, price, title, image, salePrice, quantity } = this.props.item;
     return (
       <div className="shop-item">
         <Link className="pos-rel" to={'/shop/item/' + _id}>
           <img
+            className={quantity === 0 ? 'opacity' : ''}
             src={require(`../static/shop/${image}3.jpg`).default}
             alt={title}
           />
           <h5 className="item-title">{title}</h5>
-          <Price salePrice={salePrice}>{price}</Price>
+          {quantity === 0 ? (
+            <span>
+              <strong>Out of Stock</strong>
+            </span>
+          ) : (
+            <Price salePrice={salePrice}>{price}</Price>
+          )}
+
           {salePrice && <span className="sale">Sale</span>}
         </Link>
       </div>
